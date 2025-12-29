4806ebcd1634caa202ffe522da6de196c6e141fc
Nguyễn Thanh Hiệp <thanhhiep.hongduc@gmail.com>
2025-12-29 23:41:59 +0700

    dependency injection

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,18 @@
 package com.example.demo;
+import org.springframework.context.ApplicationContext;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// @SpringBootApplication
+@SpringBootApplication(
+	exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+		ApplicationContext abc = SpringApplication.run(DemoApplication.class, args);
 
+
+}
 }
---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.UserService;
+
+
+@RestController
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String getUser() {
+        return this.userService.getUserInfo();
+    }
+}
